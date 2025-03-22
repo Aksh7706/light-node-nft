39e0ed84a28e3c3a812b1a415734578f17d131b1
Josh Dechant <josh@codecrafting.org>
2025-03-22 14:36:34 -0400

    Add onchain metadata

--- contracts/LeapLightNode.sol
+++ contracts/LeapLightNode.sol
@@ -1,58 +1,53 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.24;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Updatable} from "@forma-dev/sdk/contracts/token/ERC721/ERC721Updatable.sol";
+import {ERC721 as ERC721OZ} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721Royalty} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ITokenMetadata} from "@forma-dev/sdk/contracts/interfaces/metadata/ITokenMetadata.sol";
 
-contract LeapLightNode is ERC721, ERC721Enumerable, Ownable, EIP712 {
+contract LeapLightNode is ERC721Updatable, ERC721Enumerable, Ownable, EIP712 {
     uint256 private _nextTokenId;
     address private _signerAddress;
-    
+
     // Add mapping for token stages
     mapping(uint256 => bytes1) private _tokenStages;
-    
-    // Constants for stages
-    bytes1 private immutable STAGE_1;
-    bytes1 private immutable STAGE_2;
-    bytes1 private immutable STAGE_3;
 
-    // Metadata URIs for different stages
-    mapping(bytes1 => string) private _stageMetadataURIs;
+    // Constants for stages
+    bytes1 private constant STAGE_1 = 0x01;
+    bytes1 private constant STAGE_2 = 0x02;
+    bytes1 private constant STAGE_3 = 0x03;
 
     bytes32 private constant MINT_TYPEHASH = keccak256("Mint(address to)");
 
     // Events for important state changes
     event TokenStageUpdated(uint256 indexed tokenId, bytes1 stage);
     event SignerAddressUpdated(address indexed oldSigner, address indexed newSigner);
-    event StageMetadataURIUpdated(bytes1 indexed stage, string uri);
+    event StageMetadataUpdated(bytes1 indexed stage, string metadata);
 
     constructor(
         address initialOwner,
         address signerAddress,
-        string[] memory stageURIs
+        string[] memory stageMetadata
     )
-        ERC721("LeapLightNode", "LLN")
+        ERC721OZ("LeapLightNode", "LLN")
         EIP712("LeapLightNode", "1.0")
         Ownable(initialOwner)
     {
         require(signerAddress != address(0), "Signer cannot be zero address");
-        require(stageURIs.length == 3, "Must provide exactly 3 stage URIs");
-        
+        require(stageMetadata.length == 3, "Must provide exactly 3 stage metadata blobs");
+
         _signerAddress = signerAddress;
 
-        // Set stage constants
-        STAGE_1 = 0x01;
-        STAGE_2 = 0x02;
-        STAGE_3 = 0x03;
-        
-        // Set metadata URI for each stage from the array
-        for (uint8 i = 0; i < stageURIs.length; i++) {
-            require(bytes(stageURIs[i]).length > 0, "Stage URI cannot be empty");
-            _stageMetadataURIs[bytes1(i + 1)] = stageURIs[i];
+        // Set metadata for each stage from the array
+        for (uint8 i = 0; i < stageMetadata.length; i++) {
+            require(bytes(stageMetadata[i]).length > 0, "Stage metadata cannot be empty");
+            _setTokenMetadataForced(_getTokenMetadataKey(bytes1(i + 1)), stageMetadata[i]);
         }
     }
 
@@ -76,29 +71,13 @@ contract LeapLightNode is ERC721, ERC721Enumerable, Ownable, EIP712 {
         _tokenStages[tokenId] = STAGE_1;
     }
 
-    // The following functions are overrides required by Solidity.
-    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
-        super._increaseBalance(account, value);
-    }
-
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
-        return super.supportsInterface(interfaceId);
-    }
-    
     // The following function override to make the NFT non-transferrable
-    function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Enumerable) returns (address) {
+    function _update(address to, uint256 tokenId, address auth) internal override(ERC721OZ, ERC721Enumerable) returns (address) {
         address from = _ownerOf(tokenId);
         // Only allow initial minting (from zero address)
         // Prevent all transfers (including burning)
         require(from == address(0), "Soulbound: NFT cannot be transferred or burned");
         return super._update(to, tokenId, auth);
-    }
-
-    // Override tokenURI to return stage-specific metadata
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        _requireOwned(tokenId);
-        bytes1 stage = _tokenStages[tokenId];
-        return _stageMetadataURIs[stage];
     }
 
     // Admin functions
@@ -110,20 +89,20 @@ contract LeapLightNode is ERC721, ERC721Enumerable, Ownable, EIP712 {
         emit SignerAddressUpdated(oldSigner, signerAddress);
     }
 
- 
-    // 2. Update stage metadata URI
-    function setStageMetadataURI(bytes1 stage, string calldata uri) external onlyOwner validateStage(stage) {
-        _stageMetadataURIs[stage] = uri;
-        emit StageMetadataURIUpdated(stage, uri);
+    // 2. Update stage metadata
+    function setStageMetadata(bytes1 stage, string calldata metadata) external onlyOwner validateStage(stage) {
+        _setTokenMetadataForced(_getTokenMetadataKey(stage), metadata);
+        emit StageMetadataUpdated(stage, metadata);
     }
-    
+
     // 3. Update token stage
     function updateTokenStage(uint256 tokenId, bytes1 stage) external onlyOwner validateStage(stage) {
         _requireOwned(tokenId);
         _tokenStages[tokenId] = stage;
         emit TokenStageUpdated(tokenId, stage);
+        emit MetadataUpdate(tokenId);
     }
-    
+
     // 4. Batch update token stage
     function batchUpdateStagesByAddress(
         address[] calldata users, 
@@ -133,12 +112,13 @@ contract LeapLightNode is ERC721, ERC721Enumerable, Ownable, EIP712 {
             address user = users[i];
             uint256 balance = balanceOf(user);
             if (balance == 0) continue; // Skip if user doesn't own any tokens
-            
+
             // Since this is a soulbound token and users can only have one token,
             // we can get their token using the tokenOfOwnerByIndex function
             uint256 tokenId = tokenOfOwnerByIndex(user, 0);
             _tokenStages[tokenId] = stage;
             emit TokenStageUpdated(tokenId, stage);
+            emit MetadataUpdate(tokenId);
         }
     }
 
@@ -155,5 +135,33 @@ contract LeapLightNode is ERC721, ERC721Enumerable, Ownable, EIP712 {
         require(balance > 0, "Address does not own any tokens");
         uint256 tokenId = tokenOfOwnerByIndex(user, 0);
         return uint8(_tokenStages[tokenId]);
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _increaseBalance(address account, uint128 value) internal override(ERC721OZ, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721Royalty, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721OZ, ERC721Updatable) returns (string memory) {
+        return _uri(tokenId);
+    }
+
+    function _getTokenMetadataKey(uint256 tokenId) internal view override returns (bytes32) {
+        return _getTokenMetadataKey(_tokenStages[tokenId]);
+    }
+
+    function _getTokenMetadataKey(bytes1 stage) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("LeapLightNode", stage));
+    }
+
+    /// @dev Returns whether token metadata can be set in the given execution context.
+    function _canSetTokenMetadata(uint256) internal view override returns (bool) {
+        return owner() == _msgSender();
     }
 }
